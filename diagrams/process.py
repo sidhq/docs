1143e990a9d42cd297fdb95483b08ac0f5076448
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-11-16 09:33:07 +0100

    process svgs

--- diagrams/process.py
+++ diagrams/process.py
@@ -5,7 +5,8 @@ def replace_fill_attribute(pattern):
     for filename in glob.glob(pattern):
         with open(filename, 'r+') as file:
             content = file.read()
-            content_new = re.sub(r'fill=".*?"', '', content)
+            content_new = re.sub(r'(<rect[^>]*?)fill=".*?"', r'\1', content)
+            content_new = re.sub(r'(<rect[^>]*?)stroke="black"', r'\1', content_new)
             file.seek(0)
             file.write(content_new)
             file.truncate()
